df9a7c08d1aa25a312f1827f348225df22f34ebc
Reinhard Hainz <46258343+haipdev@users.noreply.github.com>
2025-09-22 21:17:58 +0000

    add colors and minimap

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { BpmnModeler } from './bpmn-modeler/bpmn-modeler';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    BpmnModeler
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bpmn-modeler/empty-types.ts
@@ -0,0 +1,9 @@
+declare module 'bpmn-js-color-picker' {
+    const noTypesYet: any;
+    export default noTypesYet;
+}
+
+declare module 'diagram-js-minimap' {
+    const noTypesYet: any;
+    export default noTypesYet;
+}
